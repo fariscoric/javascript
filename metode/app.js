7f27ad15e9a16e871af5c7081c5cc020b0432129
fariscoric <100435555+fariscoric@users.noreply.github.com>
2022-05-31 00:51:02 +0800

    Add files via upload

---
+++ metode/app.js
@@ -0,0 +1,72 @@
+// const DATA1 = [100,65,76,80,99,53,1,2,44];
+
+// const returnEven = (numbers) => {
+//     return (even = numbers.filter(num => num % 2 === 0 && num <=50));
+// };
+
+//console.log(returnEven(DATA1));
+
+
+
+
+// const DATA2 = [5,3,2,8,7,9];
+// const returnPow = (numbers) => {
+//     return (power = numbers.map(num => num*num+10));
+// }
+
+//console.log(returnPow(DATA2));
+
+
+
+
+// const DATA3 = [100,200,250,400,520];
+
+// const cartTotal = DATA3.reduce(
+
+    // (previousValue, currentValue) => previousValue+currentValue,500
+// );
+
+// console.log(cartTotal);
+
+
+
+
+
+// const DATA4 = [26,18,16,22,30,28];
+
+// const sumAvgAge = (ages) => {
+    // const getAverage = 
+    // ages.reduce((prevValue, currValue) => prevValue+currValue)/ages.length;
+
+    // return parseFloat(getAverage.toFixed(2));
+// }
+    // console.log(sumAvgAge(DATA4));
+
+
+
+
+    // const DATA5 = [51,30,39,29,10,31];
+    // console.log(DATA5.every(el => el < 55));
+
+
+
+    // const DATA6 = [51,30,39,29,10,31];
+    // console.log(DATA6.some(el => el > 50));
+
+
+
+
+    // const DATA7 = ['cat','dog','bat'];
+    // console.log(DATA7.includes('cat'));
+    // console.log(DATA7.includes('at'));
+
+
+
+    // const DATA8 = ['Fire','Air','Water'];
+    // console.log(DATA8.join('-'));
+
+
+
+
+
+
