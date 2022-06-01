0da710039ab5b5b9095a1b5858c19a12f28b3cc4
fariscoric <100435555+fariscoric@users.noreply.github.com>
2022-06-02 00:07:24 +0800

    Add files via upload

---
+++ nizovi3/app.js
@@ -0,0 +1,39 @@
+// const miles = [15,20,1,60,45,120]
+
+// const milesToKM = (mile) => {
+//     const kilometers = mile.map(num => num*1.60);
+// return (totalDistanceInKilometers = kilometers.reduce((prevValue, currValue) => prevValue+currValue));
+// }
+
+// console.log(milesToKM(miles));
+
+
+
+
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const compute = (number) => {
+//     const even = number.filter((num) => num % 2 === 0).map(num => num * num);
+//     return evenSquareSum = even.reduce((prevValue, currValue) => prevValue+currValue);
+// }
+
+// console.log(compute(numbers));
+
+
+
+
+
+
+let strings = ["avengers", "captain america", "ironman", "black panther"];
+
+const capitalize = (string) => {
+    const capital = string.map(letter => letter.toUpperCase());
+    return capital;
+}
+
+console.log(capitalize(strings));
+
+
+
+
